31b18c36c7d9ab510b30e5508356e70f5529f838
= <=>
2024-10-12 11:01:33 +0530

    improve prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -36,7 +36,7 @@ export const runtime = "nodejs";
 
 const customTemplate = `Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
+Keep the answer as concise as possible.
 Always say "thanks for asking!" at the end of the answer. Use emojis whenever needed.
 
 {context}
